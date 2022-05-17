a62f3772f41ed1fd4580a2fb39ba309195ecfa33
Nico <nicosurguladze@gmail.com>
2022-05-17 19:07:49 +0400

    added skills section and fade effects on scroll

---
+++ home-page/script.js
@@ -0,0 +1,26 @@
+
+document.addEventListener("scroll", () => {
+    fade(615, "start-page");
+    fade(780, "projects-title");
+    fade(1313, "projects-cards");
+    fade(1445, "button-block");
+});
+
+
+function fade(scrollPos, elId) {
+    if ( window.pageYOffset > scrollPos) {
+        document.getElementById(elId).style.visibility = "hidden";
+        document.getElementById(elId).style.opacity = 0;
+        document.getElementById(elId).style.transition = "visibility 0s 1s, opacity 1s linear";
+
+
+    } else {
+        document.getElementById(elId).style.visibility = "visible";
+        document.getElementById(elId).style.opacity = 1;
+        document.getElementById(elId).style.transition = "opacity 1s linear";
+
+    }
+    console.log(window.pageYOffset);
+}
+
+
